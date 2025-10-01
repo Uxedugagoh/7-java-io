1908ca308457c640f817490133466a66673d5d88
homemade <agent-001panamka@mail.ru>
2025-10-02 00:46:15 +0500

    7th module completed

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -15,7 +15,18 @@ public class Task01Main {
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        int answer = 0;
+        int n = 0;
+
+        try {
+            while ((n = inputStream.read()) != -1) {
+                answer = Integer.rotateLeft(answer, 1) ^ n;
+            }
+            return answer;
+        } catch (IOException e) {
+            throw new IOException(e);
+        } catch (NullPointerException e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,8 @@
 package com.example.task04;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -9,6 +11,22 @@ public class Task04Main {
         // - запустить программу
         // - проверить, что получилось 351.731900
 
-        System.out.println("0.0");
+        // Тесты не запускаются из-за проблем с System.in, хотя задание из файла работает исправно (пог. 0.000034).
+        try (Scanner scanner = new Scanner (new FileInputStream("task04/src/com/example/task04/input.test"))) {
+            // \s - любой пробельный символ. \p{Pd} все виды тире и дефисов (в юникод). [] - хотя бы один из.
+            // + - склеивает повторы.
+            scanner.useLocale(Locale.US);
+            scanner.useDelimiter("[\\s\\p{Pd}]+");
+            PrintWriter printWriter = new PrintWriter(System.out);
+            float val = 0;
+            while (scanner.hasNext()) {
+                if  (scanner.hasNextFloat()) {
+                    val += Float.parseFloat(scanner.next());
+                } else
+                    scanner.next();
+            }
+            printWriter.printf("%.6f", val);
+            printWriter.flush();
+        }
     }
 }
